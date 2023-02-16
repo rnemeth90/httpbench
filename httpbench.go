004ced18e9495b409cbe474904d985deebe02ad3
ryan nemeth <ryannemeth@live.com>
2023-02-16 13:28:51 -0500

    updating make request

--- httpbench.go
+++ httpbench.go
@@ -2,42 +2,49 @@ package httpbench
 
 import (
 	"net/http"
+	"sync"
 	"time"
 )
 
 type HTTPResponse struct {
 	Latency time.Duration
 	Status  int
+	Err     error
 }
 
 // MakeRequest makes a HTTP request
-func MakeRequest(url string) (*HTTPResponse, error) {
+func MakeRequest(url string, rChan chan HTTPResponse, mu *sync.Mutex, wg *sync.WaitGroup) {
+	defer mu.Unlock()
+	defer wg.Done()
 
 	client := http.Client{
 		Timeout: 10 * time.Second,
 	}
 
+	httpResponse := HTTPResponse{}
+
 	request, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return nil, err
+		httpResponse.Err = err
 	}
 
+	mu.Lock()
 	start := time.Now()
 	response, err := client.Do(request)
 	if err != nil {
-		return nil, err
+		httpResponse.Err = err
 	}
 	defer response.Body.Close()
 
 	end := time.Since(start)
 	status := response.StatusCode
 
-	httpResponse := HTTPResponse{
+	httpResponse = HTTPResponse{
 		Latency: end,
 		Status:  status,
 	}
 
-	return &httpResponse, nil
+	rChan <- httpResponse
 }
 
 func parseURL(url string) string {
