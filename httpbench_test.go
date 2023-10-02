e62bac374f34060c49a7541e37b5324638af49be
ryan nemeth <ryannemeth@live.com>
2023-10-02 16:54:07 -0400

    adding some tests

--- httpbench_test.go
+++ httpbench_test.go
@@ -1,93 +1,58 @@
 package httpbench
 
 import (
+	"net/http"
 	"testing"
+	"time"
 )
 
 func TestCreateHTTPClient(t *testing.T) {
 	testCases := []struct {
-		name        string
-		timeout     int64
-		keepalives  bool
-		compression bool
+		name              string
+		timeout           int64
+		keepalives        bool
+		compression       bool
+		proxyAddress      string
+		proxyUser         string
+		proxyPass         string
+		skipSSLValidation bool
 	}{
-		{name: "KeepAlivesAndCompression", timeout: 10, keepalives: true, compression: true},
+		{name: "KeepAlivesAndCompression", timeout: 10, keepalives: true, compression: true, proxyAddress: "", proxyUser: "", proxyPass: "", skipSSLValidation: false},
+		{name: "KeepAlivesAndNoCompression", timeout: 10, keepalives: true, compression: false, proxyAddress: "", proxyUser: "", proxyPass: "", skipSSLValidation: false},
+		{name: "NoKeepAlivesAndCompression", timeout: 10, keepalives: false, compression: true, proxyAddress: "", proxyUser: "", proxyPass: "", skipSSLValidation: false},
+		{name: "NoKeepAlivesAndNoCompression", timeout: 10, keepalives: false, compression: false, proxyAddress: "", proxyUser: "", proxyPass: "", skipSSLValidation: false},
+		{name: "NoKeepAlivesAndNoCompressionSkipSSLValidation", timeout: 10, keepalives: false, compression: false, proxyAddress: "", proxyUser: "", proxyPass: "", skipSSLValidation: true},
 	}
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			got := createHTTPClient(int64(test.timeout), test.keepalives, test.compression, "", "", "", true)
 
-			if int64(got.Timeout) != test.timeout {
+			expectedDuration := time.Duration(test.timeout) * time.Second
+			if got.Timeout != expectedDuration {
 				t.Errorf("expected: %v\ngot: %v", test.timeout, got.Timeout)
 			}
 		})
 	}
 }
 
-// func TestParseURL(t *testing.T) {
-// 	testCases := []struct {
-// 		name    string
-// 		url     string
-// 		useHTTP bool
-// 		want    string
-// 	}{
-// 		{name: "secureURL", url: "google.com", useHTTP: false, want: "https://google.com"},
-// 		{name: "insecureURL", url: "google.com", useHTTP: true, want: "http://google.com"},
-// 	}
+func TestDispatcher(t *testing.T) {
+	duration := 2
+	rps := 5 // Requests per second
+	expectedTotalRequests := duration * rps
 
-// 	for _, test := range testCases {
-// 		t.Run(test.name, func(t *testing.T) {
-// 			got := httpbench.ParseURL(test.url, test.useHTTP)
+	reqChan := make(chan *http.Request, expectedTotalRequests)
 
-// 			if got != test.want {
-// 				t.Errorf("expected: %s\ngot: %s", test.want, got)
-// 			}
-// 		})
-// 	}
-// }
+	// You might want to test different configurations using table driven tests.
+	// Here's an example for a single configuration.
+	Dispatcher(reqChan, duration, rps, "https://example.com", "GET", nil, "", "", "")
 
-// func TestParseHeaders(t *testing.T) {
-// 	testCases := []struct {
-// 		name         string
-// 		headerString string
-// 	}{
-// 		{name: "test1", headerString: "server:aws,x-sid:apollo"},
-// 	}
+	count := 0
+	for range reqChan {
+		count++
+	}
 
-// 	for _, test := range testCases {
-// 		t.Run(test.name, func(t *testing.T) {
-// 			want := make(map[string]string)
-// 			want["server"] = "aws"
-// 			want["x-sid"] = "apollo"
-
-// 			got := httpbench.ParseHeaders(test.headerString)
-
-// 			if !reflect.DeepEqual(got, want) {
-// 				t.Errorf("expected: %v\ngot: %v", want, got)
-// 			}
-// 		})
-// 	}
-// }
-
-// func TestMakeRequestAsync(t *testing.T) {
-// 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Add("host", "tester")
-// 		w.WriteHeader(http.StatusTeapot)
-// 	}))
-
-// 	results := []httpbench.HTTPResponse{}
-// 	client := http.Client{}
-// 	var mu sync.Mutex
-// 	var wg sync.WaitGroup
-
-// 	wg.Add(1)
-// 	// httpbench.MakeRequestAsync(server.URL, false, "", nil, &mu, &wg, &client, &results)
-
-// 	// populate a slice of httpbench.HTTPResponse (named "expect") with our expected results
-
-// 	// compare 'results' with 'expect'
-
-// 	// need table tests to test various cases
-
-// }
+	if count != expectedTotalRequests {
+		t.Errorf("Expected %d requests, but got %d", expectedTotalRequests, count)
+	}
+}
